ca3ebb7b93f7b84312ba8e0dc41a397d6d86c26c
Kirill <seeraydary@gmail.com>
2024-08-24 07:37:29 +0300

    chapter-06.01-checking-properties.03-summary

---
+++ chapter-06/01-checking-properties/03-summary.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Summary = React.createClass({
+    displayName: "Summary",
+    propTypes: {
+      /*
+          Если в компонент Summary не поступит никаких свойств, то будет ошибка, так как ingredients получит тип
+          undefined, который не имеет свойство length. isRequired указывается для данного случая. Теперь при отображении
+          Summary без свойств в консоль выведется предупреждение перед возникновением ошибки. Будет проще выяснить
+          причину сбоя
+      */
+      ingredients: PropTypes.array.isRequired,
+      steps: PropTypes.array.isRequired,
+      title: PropTypes.string,
+    },
+    render() {
+        const {ingredients, steps, title} = this.props
+        return (
+            <div className="summary">
+                <h1>{title}</h1>
+                <p>
+                    <span>{ingredients.length} Ingredients</span> |
+                    <span>{steps.length} Steps</span>
+                </p>
+            </div>
+        )
+    }
+})
