20fba0bb81117ba9823cdc5f1ab9d30b007a5293
Kirill <seeraydary@gmail.com>
2024-08-24 08:09:31 +0300

    chapter-06.01-checking-properties.10-summary

---
+++ chapter-06/01-checking-properties/10-summary.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {PropTypes}  from "react";
+
+class Summary extends React.Component {
+
+    // Статические свойства класса позволяют инкапсулировать propTypes и defaultProps
+    static propTypes = {
+        ingredients: PropTypes.number.isRequired,
+        steps: PropTypes.number.isRequired,
+        title: (props, propName) =>
+            (typeof props[propName] !== 'string') ?
+                new Error("A title must be a string") :
+                (props[propName].length > 20) ?
+                    new Error("Title is over 20 characters") :
+                    null
+    }
+
+    static defaultProps = {
+        ingredients: 0,
+        steps: 0,
+        title: "[recipe]"
+    }
+
+    render() {
+        const {ingredients, steps, title} = this.props
+        return (
+            <div className="summary">
+                <h1>{title}</h1>
+                <p>
+                    <span>{ingredients} Ingredients</span> | <span>{steps} Steps</span>
+                </p>
+            </div>
+        )
+    }
+}
